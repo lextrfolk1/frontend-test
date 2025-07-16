eef1f451aa4b855569f7ede90111b899291f515f
lextrfolk1 <lextrfolk1@gmail.com>
2025-07-16 23:30:35 +0530

    tetsing ghateway

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [responseHtml, setResponseHtml] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://gateway-service';
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://34.56.192.186';
 
   const callApi = async (endpoint: string) => {
     setLoading(true);
